5a700f8e436f3806cc2dcababc1027ef53b4a9df
Patrick Hislop <pfh28@drexel.edu>
2017-05-10 18:45:32 -0400

    Set up code in IDE

--- src/MacroMain.java
+++ src/MacroMain.java
@@ -6,7 +6,7 @@ public class MacroMain
 	{
 		Typist typist = new Typist();
 		MacroParser parser = new MacroParser();
-		ArrayList<int[]> cmds = parser.parseFile("test");
+		ArrayList<int[]> cmds = parser.parseFile("test.txt");
 
 		typist.type(cmds);
 
