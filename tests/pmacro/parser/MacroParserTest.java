a6e4ee0cc695028fe209073e899de1b835588936
root <root@localhost.localdomain>
2017-05-11 16:52:48 -0400

    commit of incomplete initial testing work

---
+++ tests/pmacro/parser/MacroParserTest.java
@@ -0,0 +1,20 @@
+package pmacro.parser;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MacroParserTest extends TestCase
+{
+    protected void setUp()
+    {
+        MacroParser parser = new MacroParser();
+    }
+
+    @Test
+    public void parseLineTest() throws Exception
+    {
+
+    }
+}
